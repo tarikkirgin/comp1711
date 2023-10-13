140081c22685064602d1a5984460a021fa1a9838
tarik <123542696+tarikkirgin@users.noreply.github.com>
2023-10-13 15:36:44 +0000

    add newline

--- C/Session_2/for_first.c
+++ C/Session_2/for_first.c
@@ -6,5 +6,5 @@ int main() {
     for(a = 1; a <= 5; a++){
         printf("a = %d\n", a);
     }
-    printf("After exiting hte loop: a = %d", a);
+    printf("After exiting hte loop: a = %d\n", a);
 }
