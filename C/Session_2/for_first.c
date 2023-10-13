6a197474e6944110c2827b865ba887c6b237c174
tarik <123542696+tarikkirgin@users.noreply.github.com>
2023-10-13 15:31:23 +0000

    fix

--- C/Session_2/for_first.c
+++ C/Session_2/for_first.c
@@ -3,8 +3,8 @@
 int main() {
     int a;
 
-    for(a = 1; a <= 5; a++2){
+    for(a = 1; a <= 5; a++){
         printf("a = %d\n", a);
     }
-    printf("After exiting hte loop: a = %d\n", a);
+    printf("After exiting hte loop: a = %d", a);
 }
