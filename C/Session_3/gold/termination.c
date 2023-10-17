7e586743c7eb69017fdd45430855f9f750b169b7
tarik <123542696+tarikkirgin@users.noreply.github.com>
2023-10-17 15:28:28 +0000

    refactor for better coding practices

--- C/Session_3/gold/termination.c
+++ C/Session_3/gold/termination.c
@@ -4,20 +4,19 @@ int main()
 {
     int termination_value = -1;
     int input = 0;
-    while(input != termination_value)
+    while (input != termination_value)
     {
         printf("Input a number: ");
         scanf("%d", &input);
-        if(input == termination_value){
-            printf("Termination value received.\n");
-            break;
-        }
-        if(input >= 0 && input <= 100){
+        if (input >= 0 && input <= 100)
+        {
             printf("The number is within the range.\n");
         }
-        else{
+        else if (input != termination_value)
+        {
             printf("The number is not within the range.\n");
         }
     }
+    printf("Termination value received.\n");
     return 0;
 }
